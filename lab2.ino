96fd15c7ab39c97dfac6c22a9c53e9e2923598d4
KorobovaElena <elen0603.ek@gmail.com>
2019-12-14 13:49:01 +0300

    реализовала дальнометр

--- lab2.ino
+++ lab2.ino
@@ -1,9 +1,41 @@
+const int sensorPin = A0;
+const float voltsPerMeasurement = 5.0/1024.0;
+
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float dist = getDist();
+  Serial.println(currentDist);
 
+}
+
+float getDist(){
+  float volts = readAnalog() * voltsPerMeasurement;
+  return pow(14.7737/volts, 1.2134); // for small IR sensor (SHARP 2Y0A21)
+}
+
+float readAnalog()
+{
+  int sum = 0;
+  int maxV = -5000;
+  int minV = 5000;
+  int n = 15;
+  for (int i = 0; i < n; i++)
+  {
+    int cur = analogRead(sensorPin);
+    if (cur > maxV)
+    {
+      maxV = cur;
+    }
+    if (cur < minV)
+    {
+      minV = cur;
+    }
+    sum += cur;
+    delay(6);
+  }
+  return (sum - maxV - minV) / (float)(n - 2);
 }
