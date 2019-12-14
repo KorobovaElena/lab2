b9ae9c4b36ecdaf49f3c13367cea4f8c7db17118
KorobovaElena <elen0603.ek@gmail.com>
2019-12-14 14:08:37 +0300

    реализовала работу сервопривода

--- lab2.ino
+++ lab2.ino
@@ -1,15 +1,33 @@
+#include <Servo.h>
+
+const int servoPin = 10;
 const int sensorPin = A0;
 const float voltsPerMeasurement = 5.0/1024.0;
+const int delta = 5;
+const int servoMin = 0;
+const int servoMax = 90;
+
+Servo servo;
+
+int sweetDist;
+
+bool servoIncreasing = true;
 
 void setup() {
-    Serial.begin(115200);
-
+  Serial.begin(115200);
+  sweetDist = 10;
+  servo.attach(servoPin);
+  servo.write(servoMin);
+  delay(500); 
 }
 
 void loop() {
   float dist = getDist();
-  Serial.println(currentDist);
-
+  Serial.println('Текущая дистанция = ' + dist);
+  if (dist <= sweetDist + delta)
+    servo.write(servoMax);
+  else
+    servo.write(servoMin);
 }
 
 float getDist(){
